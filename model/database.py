4778065ff0816ed43b4c17678c58bf2fbb343838
Lucca Giovane Gomes <55901179+LuccaGiovane@users.noreply.github.com>
2025-01-06 11:46:56 -0300

    Projeto 1 PSW

---
+++ model/database.py
@@ -0,0 +1,9 @@
+from .model import *
+
+sqlite_file_name = 'database.db'
+sqlite_url = f'sqlite:///{sqlite_file_name}'
+
+engine = create_engine(sqlite_url, echo=True)
+
+if __name__ == '__main__':
+    SQLModel.metadata.create_all(engine)
